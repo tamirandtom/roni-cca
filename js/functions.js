02910484f88488c881519cd5630e885bdced7d4e
tamirandtom <tamirp@gmail.com>
2017-07-09 17:58:04 +0300

    fixed background cover

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,45 @@
 $(document).ready(function () {
 
+    $(".cca__modal-title").click(function () {
+        $(this).siblings(".cca__modal-conetnt").slideToggle();
+    });
 
+    function exitDialog() {
+        $('.cca__modal-wrap').fadeOut(400);
+    }
+    function showDialog() {
+        $('.cca__modal-wrap').fadeIn(400);
+    }
+
+    function showMainPage() {
+        $('.cca-page-content-wrap').fadeIn(400);
+        $('#innitialAlert').fadeOut(400);
+
+    }
+
+    function donateButton() {
+        $('.cca-page-content-wrap').fadeOut(400);
+        $('#thankYouMsg').fadeIn(400);
+
+    }
+    function cancelButton() {
+        $('.cca-page-content-wrap').fadeOut(400);
+        $('#noDonationMsg').fadeIn(400);
+
+    }
+
+
+    $("#backDialog").on("click", exitDialog);
+    $("#learnMoreButton").on("click", showDialog);
+    $("#okButtonDialog").on("click", showMainPage);
+
+    $("#dontDonateButton").on("click", cancelButton);
+    $("#donateButton").on("click", donateButton);
+    $(window).on('resize', resize);
     var screensize = {
         w: window.innerWidth,
         h: window.innerHeight
     }
-
-
     // Renderer
     var renderer = PIXI.autoDetectRenderer(screensize.w, screensize.h);
     $('#pixi-container').append(renderer.view);
@@ -20,14 +53,37 @@ $(document).ready(function () {
     var loader = new PIXI.AssetLoader(["../images/background-image.png"]);
     loader.onComplete = setup;
     loader.load();
-var displacementFilte;
-    function setup() { 
+    var displacementFilte, bg;
+
+    function resize() {
+        var screensize = {
+            w: window.innerWidth,
+            h: window.innerHeight
+        }
+        if (screensize.w > screensize.h) {
+            var ratio =  screensize.w / screensize.h;
+            bg.width = screensize.w
+            bg.height = ratio * screensize.w;
+            bg.position.y =  -(bg.height - bg.width)/2;
+            
+        } 
+        else {
+            var ratio =  screensize.h / screensize.w;
+            bg.width = ratio * screensize.h;
+            bg.height = screensize.h;
+            bg.position.x =  -(bg.width - bg.height)/2;
+        }
+
+        renderer.resize(screensize.w, screensize.h);
+
+    }
+
+    function setup() {
         var texture = PIXI.TextureCache["../images/background-image.png"];
-        var bg = new PIXI.Sprite(texture);
+        bg = new PIXI.Sprite(texture);
         container.addChild(bg);
         // Stretch background
-        bg.scale.x = screensize.w/bg.width;
-        bg.scale.y = screensize.h/bg.height;
+        resize();
         // Filter
         var displacementTexture = PIXI.Texture.fromImage("http://i.imgur.com/2yYayZk.png");
         displacementFilter = new PIXI.DisplacementFilter(displacementTexture);
@@ -36,12 +92,12 @@ var displacementFilte;
     }
     // Animate
     function animate() {
-        var offset = 0.2;
+        var offset = 0.5;
         displacementFilter.offset.x += offset;
         // displacementFilter.offset.y += offset;
         renderer.render(stage);
         requestAnimFrame(animate);
-       
+
     }
 
 });
